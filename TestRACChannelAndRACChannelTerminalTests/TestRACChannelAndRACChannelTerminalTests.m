78bd73d8d20c30748520dc9f86c06c05a325e259
jiangzenghui005@163.com <jiangzenghui005@163.com>
2018-08-27 18:12:34 +0800

    测试用例提交。

--- TestRACChannelAndRACChannelTerminalTests/TestRACChannelAndRACChannelTerminalTests.m
+++ TestRACChannelAndRACChannelTerminalTests/TestRACChannelAndRACChannelTerminalTests.m
@@ -8,32 +8,283 @@
 
 #import <XCTest/XCTest.h>
 
+#import <ReactiveCocoa.h>
+
 @interface TestRACChannelAndRACChannelTerminalTests : XCTestCase
 
 @end
 
 @implementation TestRACChannelAndRACChannelTerminalTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void)test_channel1
+{
+    RACChannel *channel = [[RACChannel alloc] init];
+    
+    [channel.leadingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- leading -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- leading -- error");
+    } completed:^{
+        NSLog(@"channel -- leading -- completed");
     }];
+    
+    [channel.followingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- following -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- following -- error");
+    } completed:^{
+        NSLog(@"channel -- following -- completed");
+    }];
+    
+    RACSignal *signal1 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(1)];
+        [subscriber sendCompleted];
+        
+        return nil;
+    }];
+    
+    RACSignal *signal2 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(2)];
+        [subscriber sendError:nil];
+        
+        return nil;
+    }];
+    
+    [signal1 subscribe:channel.leadingTerminal];
+    [signal2 subscribe:channel.followingTerminal];
+    
+    // 打印日志：
+    /*
+     2018-08-27 17:57:20.531682+0800 TestRACChannelAndRACChannelTerminal[2721:4239128] channel -- following -- 1
+     2018-08-27 17:57:20.532169+0800 TestRACChannelAndRACChannelTerminal[2721:4239128] channel -- leading -- completed
+     2018-08-27 17:57:20.532710+0800 TestRACChannelAndRACChannelTerminal[2721:4239128] channel -- following -- completed
+     */
+}
+
+- (void)test_channel2
+{
+    RACChannel *channel = [[RACChannel alloc] init];
+    
+    RACSignal *signal1 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(1)];
+        [subscriber sendCompleted];
+        
+        return nil;
+    }];
+    
+    RACSignal *signal2 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(2)];
+        [subscriber sendError:nil];
+        
+        return nil;
+    }];
+    
+    [signal1 subscribe:channel.leadingTerminal];
+    
+    [channel.leadingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- leading -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- leading -- error");
+    } completed:^{
+        NSLog(@"channel -- leading -- completed");
+    }];
+    
+    [channel.followingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- following -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- following -- error");
+    } completed:^{
+        NSLog(@"channel -- following -- completed");
+    }];
+    
+    [signal2 subscribe:channel.followingTerminal];
+    
+    // 打印日志：
+    /*
+     2018-08-27 17:58:31.369902+0800 TestRACChannelAndRACChannelTerminal[2783:4242954] channel -- leading -- completed
+     2018-08-27 17:58:31.370140+0800 TestRACChannelAndRACChannelTerminal[2783:4242954] channel -- following -- 1
+     2018-08-27 17:58:31.370276+0800 TestRACChannelAndRACChannelTerminal[2783:4242954] channel -- following -- completed
+     */
+}
+
+- (void)test_channel3
+{
+    RACChannel *channel = [[RACChannel alloc] init];
+    
+    RACSignal *signal1 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(1)];
+        [subscriber sendCompleted];
+        
+        return nil;
+    }];
+    
+    RACSignal *signal2 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(2)];
+        [subscriber sendError:nil];
+        
+        return nil;
+    }];
+    
+    [signal1 subscribe:channel.leadingTerminal];
+    
+    [signal2 subscribe:channel.followingTerminal];
+    
+    [channel.leadingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- leading -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- leading -- error");
+    } completed:^{
+        NSLog(@"channel -- leading -- completed");
+    }];
+    
+    [channel.followingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- following -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- following -- error");
+    } completed:^{
+        NSLog(@"channel -- following -- completed");
+    }];
+    
+    // 打印日志：
+    /*
+     2018-08-27 18:02:56.176628+0800 TestRACChannelAndRACChannelTerminal[2962:4256112] channel -- leading -- completed
+     2018-08-27 18:02:56.176879+0800 TestRACChannelAndRACChannelTerminal[2962:4256112] channel -- following -- 1
+     2018-08-27 18:02:56.177093+0800 TestRACChannelAndRACChannelTerminal[2962:4256112] channel -- following -- completed
+     */
+}
+
+- (void)test_channel11
+{
+    RACChannel *channel = [[RACChannel alloc] init];
+    
+    [channel.leadingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- leading -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- leading -- error");
+    } completed:^{
+        NSLog(@"channel -- leading -- completed");
+    }];
+    
+    [channel.followingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- following -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- following -- error");
+    } completed:^{
+        NSLog(@"channel -- following -- completed");
+    }];
+    
+    RACSignal *signal1 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(1)];
+        [subscriber sendCompleted];
+        
+        return nil;
+    }];
+    
+    RACSignal *signal2 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(2)];
+        [subscriber sendError:nil];
+        
+        return nil;
+    }];
+    
+    [signal2 subscribe:channel.followingTerminal];
+    [signal1 subscribe:channel.leadingTerminal];
+    
+    // 打印日志：
+    /*
+     2018-08-27 18:04:01.792470+0800 TestRACChannelAndRACChannelTerminal[3019:4259803] channel -- leading -- 2
+     2018-08-27 18:04:01.792733+0800 TestRACChannelAndRACChannelTerminal[3019:4259803] channel -- following -- error
+     2018-08-27 18:04:01.792859+0800 TestRACChannelAndRACChannelTerminal[3019:4259803] channel -- leading -- error
+     */
+}
+
+- (void)test_channel12
+{
+    RACChannel *channel = [[RACChannel alloc] init];
+    
+    RACSignal *signal1 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(1)];
+        [subscriber sendCompleted];
+        
+        return nil;
+    }];
+    
+    RACSignal *signal2 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(2)];
+        [subscriber sendError:nil];
+        
+        return nil;
+    }];
+    
+    [signal2 subscribe:channel.followingTerminal];
+    
+    [channel.leadingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- leading -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- leading -- error");
+    } completed:^{
+        NSLog(@"channel -- leading -- completed");
+    }];
+    
+    [channel.followingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- following -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- following -- error");
+    } completed:^{
+        NSLog(@"channel -- following -- completed");
+    }];
+    
+    [signal1 subscribe:channel.leadingTerminal];
+    
+    // 打印日志：
+    /*
+     2018-08-27 18:08:08.847385+0800 TestRACChannelAndRACChannelTerminal[3191:4272163] channel -- leading -- error
+     2018-08-27 18:08:08.847591+0800 TestRACChannelAndRACChannelTerminal[3191:4272163] channel -- following -- error
+     */
+}
+
+- (void)test_channel13
+{
+    RACChannel *channel = [[RACChannel alloc] init];
+    
+    RACSignal *signal1 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(1)];
+        [subscriber sendCompleted];
+        
+        return nil;
+    }];
+    
+    RACSignal *signal2 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(2)];
+        [subscriber sendError:nil];
+        
+        return nil;
+    }];
+    
+    [signal2 subscribe:channel.followingTerminal];
+    [signal1 subscribe:channel.leadingTerminal];
+    
+    [channel.leadingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- leading -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- leading -- error");
+    } completed:^{
+        NSLog(@"channel -- leading -- completed");
+    }];
+    
+    [channel.followingTerminal subscribeNext:^(id x) {
+        NSLog(@"channel -- following -- %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"channel -- following -- error");
+    } completed:^{
+        NSLog(@"channel -- following -- completed");
+    }];
+    
+    // 打印日志：
+    /*
+     2018-08-27 18:08:59.462893+0800 TestRACChannelAndRACChannelTerminal[3236:4274939] channel -- leading -- error
+     2018-08-27 18:08:59.466973+0800 TestRACChannelAndRACChannelTerminal[3236:4274939] channel -- following -- error
+     */
 }
 
 @end
